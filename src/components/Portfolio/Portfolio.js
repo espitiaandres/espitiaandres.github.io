35b8cf9ecf5690a0bc63a519bd03a71f52f6c410
espitiaandres <espitiaandres123@gmail.com>
2020-12-16 17:52:28 -0500

    deleted yarn.lock

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ParticlesBg from "particles-bg";
 import LandingPage from '../LandingPage/LandingPage';
 import SkillsPage from '../SkillsPage/SkillsPage';
 
 const Portfolio = () => {
     return (
-        <Router basename="/">
-            <Route path="/" exact render={() => (
-                <div>
-                    <LandingPage />
-                </div>
-            )}/>
-
+        <div>
+            <LandingPage />            
             {/*
                 <Route path="/skills" exact render={() => (
                     <SkillsPage />
                 )}/>
             */}
-
-        </Router>
+        </div>
     )
 }
 
