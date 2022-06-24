6ed979fd47d9fb96fd368b84d12e3cb9d6f5d15b
Andres Espitia <espitiaandres123@gmail.com>
2022-06-23 22:47:02 -0700

    feat: add a expand/collapse all button

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,29 +1,55 @@
+import { useState, useEffect, useRef } from "react";
 import "animate.css";
 import { achievementsList, IAchievement } from "./AchievementsStaticData.ts";
 
 const Achievements = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const inputsRef = useRef([]);
+
+  useEffect(() => {
+    inputsRef.current = inputsRef.current.slice(0, achievementsList.length);
+  }, []);
+
   return (
     <div className="animate__animated animate__fadeIn animate__delay-0.5s">
-      <div className="text-black mb-4">
-        Here are some projects and achievements I'm proud of:
+      <div className="flex flex-row justify-between">
+        <div className="text-black mb-4">
+          Here are some projects and achievements I'm proud of:
+        </div>
+        <button
+          className="btn btn-outline btn-primary -mt-2 mb-2 w-40"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+            inputsRef?.current?.map((d) => d.click());
+            // console.log(inputsRef);
+            // inputsRef?.current?.click();
+            // inputsRef?.current?.((d) => d.click)
+          }}
+        >
+          {isExpanded ? "Collapse" : "Expand"} All
+        </button>
       </div>
       {achievementsList.map(
-        ({ text, links, description, image }: IAchievement) => (
+        ({ text, links, description, image }: IAchievement, i: number) => (
           <div>
             <div
               tabindex="0"
-              class="collapse collapse-arrow rounded-box text-black border-2 hover:border-primary ease-in duration-100"
+              className="collapse collapse-arrow rounded-box text-black border-2 hover:border-primary ease-in duration-100"
             >
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                // ref={inputRef}
+                ref={(el) => ((inputsRef as any).current[i] = el)}
+              />
               <div className="collapse-title text-lg font-medium text-secondary rounded-full truncate">
                 {text}
               </div>
               <div className="collapse-content ease-in duration-100">
-                <div className="mb-2">{description}</div>
-                {links.length > 0 && (
+                <div className="mb-2">{description ?? ""}</div>
+                {links?.length > 0 && (
                   <div>
                     <div>See more:</div>
-                    {links.map((l: IAchievement) => (
+                    {links?.map((l: IAchievement) => (
                       <div className="flex flex-row space-x-2">
                         <div>-</div>
                         <a
