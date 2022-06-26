46c5530637c58bf25d5fb6dc8ca4eb93b4b0924c
Andres Espitia <espitiaandres123@gmail.com>
2022-06-25 22:03:16 -0400

    feat: remove old code

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -41,35 +41,6 @@ const Achievements = () => {
             setIsExpanded={setIsExpanded}
             i={i}
           />
-          // <div
-          //   tabIndex={0}
-          //   className="collapse collapse-arrow rounded-box text-black border-2 hover:border-primary ease-in duration-100"
-          // >
-          //   <input
-          //     type="checkbox"
-          //     ref={(el) => ((inputsRef as any).current[i] = el)}
-          //     onClick={() => {
-          //       const openedAccordions = inputsRef?.current?.every(
-          //         (d) => d?.checked
-          //       );
-          //       setIsExpanded(openedAccordions);
-          //     }}
-          //   />
-          //   <div className="collapse-title text-lg font-medium text-secondary rounded-full truncate">
-          //     {text}
-          //   </div>
-          //   <div className="collapse-content ease-in duration-100">
-          //     <div className="mb-2">{description ?? ""}</div>
-          //     {links?.length > 0 && (
-          //       <div>
-          //         <div>See more:</div>
-          //         {links?.map((l: string, i: number) => (
-          //           <Link l={l} key={i} />
-          //         ))}
-          //       </div>
-          //     )}
-          //   </div>
-          // </div>
         )
       )}
     </div>
