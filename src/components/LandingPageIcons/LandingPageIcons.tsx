853ab3ab6d4815510c6009b4809920665265b1d4
Andres Espitia <espitiaandres123@gmail.com>
2022-07-04 16:15:21 -0400

    feat: abstract icons static data

--- src/components/LandingPageIcons/LandingPageIcons.tsx
+++ src/components/LandingPageIcons/LandingPageIcons.tsx
@@ -10,48 +10,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-
-interface IIConsProp {
-  href: string;
-  fontAwesome: string[] | any;
-  label: string;
-}
-
-// TODO: add twitter, medium...?
-// TODO: move this out into the AboutMe.tsx component
-
-const iconsProps: IIConsProp[] = [
-  {
-    href: "https://github.com/espitiaandres",
-    fontAwesome: ["fab", "github-alt"],
-    label: "Github",
-  },
-  {
-    href: "https://www.linkedin.com/in/espitiaandres",
-    fontAwesome: ["fab", "linkedin-in"],
-    label: "LinkedIn",
-  },
-  {
-    href: "mailto:afespiti@edu.uwaterloo.ca",
-    fontAwesome: ["fas", "envelope"],
-    label: "Email",
-  },
-  {
-    href: "https://dribbble.com/espitiaandres",
-    fontAwesome: ["fab", "dribbble"],
-    label: "Dribbble",
-  },
-  {
-    href: "https://medium.com/@espitiaandres123",
-    fontAwesome: ["fab", "medium"],
-    label: "Medium",
-  },
-  {
-    href: "https://stackoverflow.com/users/13973201/espitiaandres",
-    fontAwesome: ["fab", "stack-overflow"],
-    label: "Stack Overflow",
-  },
-];
+import { iconsProps, IIConsProp } from "./LandingPageIconsStaticData.ts";
 
 const LandingPageIcons = () => {
   library.add(fab, fas);
@@ -75,24 +34,6 @@ const LandingPageIcons = () => {
           </div>
         </a>
       ))}
-      {/* <div className="text-black flex justify-center mb-6">{hoverText}</div>
-      <div className="flex flex-row justify-center mb-6 space-x-4">
-        {iconsProps.map((icon) => (
-          <div>
-            <a
-              href={icon.href}
-              target="_blank"
-              className="bg-white border-2 border-white text-primary rounded-full text-4xl w-14 h-14 flex justify-center hover:opacity-75"
-              rel="noopener noreferrer"
-              onMouseOver={() => setHoverText(icon.hoverText)}
-              onMouseLeave={() => setHoverText(defaultHoverText)}
-              title={icon.href}
-            >
-              <FontAwesomeIcon className="mt-2" icon={icon.fontAwesome} />
-            </a>
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };
---
+++ src/components/LandingPageIcons/LandingPageIconsStaticData.ts
@@ -0,0 +1,41 @@
+export interface IIConsProp {
+  href: string;
+  fontAwesome: string[] | any;
+  label: string;
+}
+
+// TODO: add twitter, medium...?
+// TODO: move this out into the AboutMe.tsx component
+
+export const iconsProps: IIConsProp[] = [
+  {
+    href: "https://github.com/espitiaandres",
+    fontAwesome: ["fab", "github-alt"],
+    label: "Github",
+  },
+  {
+    href: "https://www.linkedin.com/in/espitiaandres",
+    fontAwesome: ["fab", "linkedin-in"],
+    label: "LinkedIn",
+  },
+  {
+    href: "mailto:afespiti@edu.uwaterloo.ca",
+    fontAwesome: ["fas", "envelope"],
+    label: "Email",
+  },
+  {
+    href: "https://dribbble.com/espitiaandres",
+    fontAwesome: ["fab", "dribbble"],
+    label: "Dribbble",
+  },
+  {
+    href: "https://medium.com/@espitiaandres123",
+    fontAwesome: ["fab", "medium"],
+    label: "Medium",
+  },
+  {
+    href: "https://stackoverflow.com/users/13973201/espitiaandres",
+    fontAwesome: ["fab", "stack-overflow"],
+    label: "Stack Overflow",
+  },
+];
